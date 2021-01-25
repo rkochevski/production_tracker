476e939abc2c2d43cb4c51ff0d9d43ebdee7ea76
ristoneeisto <risto.kochevski@gmail.com>
2021-01-25 17:52:25 +0100

    initial commit

---
+++ src/main/java/com/example/proTrack/ProTrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.proTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proTrack/ProTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
